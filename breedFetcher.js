bb993b59553200c101640a463624d482ae1aa9ee
Calvin Chan <calvinchan87@gmail.com>
2021-03-14 23:33:48 +0000

    refactored with exported function and mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,30 @@
 const request = require('request');
-request("https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2].toString(), (error, response, body) => {
 
-  if (error !== null) {
-    console.log(error);
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
 
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  
-    // console.log(typeof body);
-    const data = JSON.parse(body);
-    // console.log(typeof data);
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
 
-    if (data[0] === undefined) {
-      console.log("Requested breed not found.");
+    if (error !== null) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+  
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    
+      // console.log(typeof body);
+      const data = JSON.parse(body);
+      // console.log(typeof data);
+  
+      if (data[0] === undefined) {
+        callback("Requested breed not found.", null);
+      } else {
+        callback(null, data[0].description);
+      }
+  
     }
+  
+  });
 
-  }
+};
 
-});
+module.exports = { fetchBreedDescription };
